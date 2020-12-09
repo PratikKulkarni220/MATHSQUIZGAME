21f2f1a5411912b7a38b85d3ef38070d172d379c
PratikKulkarni220 <73877259+PratikKulkarni220@users.noreply.github.com>
2020-12-09 22:08:49 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function enter_game(){
-    var name1= document.getElementById("input_1").value;
-     var name2= document.getElementById("input_2").value;
-    localStorage.setItem("1_username", name1);
-    localStorage.setItem("2_username", name2);
-    window.location=game_page.html;
-}
